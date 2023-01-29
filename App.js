b55dec3e321b82fa9dda2894002d5229f7faa5e8
JNPham <n.phamih@gmail.com>
2023-01-28 23:10:11 -0800

    UX SignUp page update

--- App.js
+++ App.js
@@ -4,19 +4,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "./Screens/Home";
-import Login from "./Screens/Login";
+import SignUp from "./Screens/SignUp";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator initialRouteName="SignUp">
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   )
 } 
-//<Stack.Screen name="Home" component={Home} />
-// Testing 
---
+++ Screens/SignUp.js
@@ -0,0 +1,198 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View, button, Pressable, TextInput, Image, TouchableOpacity } from 'react-native';
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, User } from 'firebase/auth';
+import { getDatabase, child, ref, set, get, update, onValue, remove } from "firebase/database";import { useState } from 'react';
+
+export default function SignUp({ navigation }) {
+    const [username, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [lastUserName, setLastUserName] = useState('');
+    
+    const auth = getAuth();
+    const db = getDatabase();
+    const LogoImage = require('../assets/squadsync.png');
+    const GoogleImage = require('../assets/google.png');
+    const AppleImage = require('../assets/apple.png');
+    const ConsoleImage = require('../assets/console.png');
+    const RankImage = require('../assets/Decor.png');
+    
+    function createAccount() {
+        //const auth = getAuth();
+        // TODO check email contains @ and is valid email
+        if (username.trim() != "" && email.trim() != "" && password.trim() != ""){
+          createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            const userId = user.uid
+            // Make sure we store our user in our database
+            storeUser(userId)
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+          })
+          navigation.navigate('Home'); // Navigate to the Home page after signing up
+        } else {
+          alert('Unable to create user, check the username, email and password fields!');    
+        }
+    }
+    
+    function storeUser(userId) {
+        // Store the user ID and basic info
+        set(ref(db, 'users/' + userId), {          
+          email: email,
+          username: username,
+          nickname: username
+        })
+        // Store into our just created user ID our group structure (dummy data)
+        set(ref(db, 'users/' + userId + '/groups/'), {          
+          defaultGroup: "defaultGroupName"
+        })
+        // Store into our just created user ID user profile structure (empty string for now)
+        set(ref(db, 'users/' + userId + '/profile/'), {          
+          profilePicUrl: ""
+        })
+        alert('User Created! Userid: ' + userId)
+    }
+    
+    // todo: Sign up with Google Account
+    function googleAcct() {
+        //code here
+    }
+    // todo: Sign up with Apple ID
+    function appleAcct() {
+        //code here
+    }
+
+    function getData() {
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `users/${username}`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                var data = snapshot.val()
+                console.log(data);
+                alert('data: ' + JSON.stringify(data))
+            } else {
+                console.log("No data available");
+            }
+        }).catch((error) => {
+            console.error(error);   
+        });  
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={LogoImage} style={styles.logo} />
+            <Image source={ConsoleImage} style={{position: 'absolute', width: 170, height: 130, left:'-10%', top:'-2%'}} />
+            <Image source={RankImage} style={{position: 'absolute', width: 210, height: 190, right: '0%', bottom:'0%'}} />
+
+            <Text style={[styles.text, {top:'26%'}]}>Register using a third party account</Text>
+            
+            <TouchableOpacity onPress={googleAcct} style={styles.google}> 
+              <Image source={GoogleImage} />
+            </TouchableOpacity>
+            
+            <TouchableOpacity onPress={appleAcct} style={styles.apple}>
+              <Image source={AppleImage} />
+            </TouchableOpacity>
+
+            <Text style={[styles.text, {top:'40%'}]}>Or simply fill out the information below</Text>
+
+            <TextInput value={username} onChangeText={(username) => { // FULLNAME --> todo: change code to Full Name
+                setName(username) 
+                setLastUserName(username) 
+                }
+            } 
+            placeholder='Enter Full Name' style={[styles.TextBoxes, {top:'45%'}]}></TextInput>
+
+            <TextInput value={email} onChangeText={(email) => { // EMAIL
+                setEmail(email)
+                }
+            } 
+            placeholder='Enter Email' style={[styles.TextBoxes, {top: '53%'}]}></TextInput>
+
+            <TextInput value={username} onChangeText={(username) => { // USERNAME 
+                setName(username) 
+                setLastUserName(username) // add this line
+                }
+            } 
+            placeholder='Enter Username' style={[styles.TextBoxes, {top: '61%'}]}></TextInput>
+
+            <TextInput value={password} onChangeText={(password) => { // PASSWORD
+                setPassword(password)
+                }
+            } 
+            placeholder='Enter Password' style={[styles.TextBoxes, {top: '69%'}]}></TextInput>
+
+            <Text style={[styles.text, {top:'80%'}]}>By signing up, you agree to our Terms and Conditions.</Text>
+
+            <Pressable style={{position: 'absolute', top: '90%'}}>
+              <Text onPress={createAccount} 
+                    style={{fontWeight: '800',
+                            fontSize: 19,
+                            lineHeight: 19,
+                            display: 'flex',
+                            alignItems: 'center',
+                            textAlign: 'center',
+                            color: '#1D91FC',
+                            letterSpacing: 0.05,}}>Sign Up</Text>
+            </Pressable> 
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    position: 'absolute',
+    width: 150,
+    height: 150,
+    top: '3%',
+  },
+  google: {
+    position: 'absolute',
+    width: 47,
+    height: 47,
+    left: 120,
+    top: '31%',
+  },
+  apple: {
+    position: 'absolute',
+    width: 47,
+    height: 47.89,
+    left: 216,
+    top: '31%',
+  },
+  text: {
+    position: 'absolute',
+    width: 348,
+    height: 42,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 15,
+    lineHeight: 19,
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+    letterSpacing: 0.05,
+    color: '#FFFFFF',
+  },
+  TextBoxes: {
+    position: 'absolute',
+    width:'90%',
+    fontSize:18,
+    alignItems: 'center',
+    textAlign: 'center',
+    padding:13,
+    backgroundColor: 'white',
+    marginVertical:10,
+    borderRadius: 35,
+  },
+});
