1c1a6e7dc52580e9b23459b7b2f97768d1415fcf
Han Pham <41082484+hanpm@users.noreply.github.com>
2023-03-04 19:52:27 -0800

    adding push notifications...hope this works with Android LOL

--- Screens/StackScreen/Notification.js
+++ Screens/StackScreen/Notification.js
@@ -1,21 +1,128 @@
-import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { getDatabase, ref, set, onValue, push, update, remove } from "firebase/database";
+// import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
+// import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { getDatabase, ref, set, onValue, push, update, remove } from "firebase/database";
 
-export default function Notification() {
-    return (
-        <View style={styles.container}>
-            <Text>Welcome to SquadSync Notification</Text>
-        </View>
-    );
+// export default function Notification() {
+//     return (
+//         <View style={styles.container}>
+//             <Text>Welcome to SquadSync Notification</Text>
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+// }); 
+
+import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState, useRef} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import storage from "@react-native-async-storage/async-storage";
+
+//1. import the library
+//2. get permission
+//3. do push notifications on button click
+//4. schedule push notifications
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true
+  })
+});
+
+export default function App() {
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    const getPermission = async () => {
+
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+        alert('Enable push notifications to use the app!');
+        await storage.setItem('expopushtoken', "");
+        return;
+        }
+        const token = (await Notifications.getExpoPushTokenAsync()).data;
+        await storage.setItem('expopushtoken', token);
+
+        if (Platform.OS === 'android') {
+          Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+          });
+        }
+    }
+
+    getPermission();
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {});
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  const onClick = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Push Notification",
+        body: "Here is an in-app notification",
+        data: { data: "Sent a Push Notifcation" }
+      },
+      trigger: {
+        seconds:1
+      }
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onClick}>
+        <Text style={styles.notification_button}>Click me to send a push notification</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-}); 
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  notification_button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#d3d3d3'
+
+  },
+});
