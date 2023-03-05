39e8d87eb6ac549ad3ae41ee842cc32554ca560b
Han Pham <41082484+hanpm@users.noreply.github.com>
2023-03-04 20:03:09 -0800

    deleting old code

--- Screens/StackScreen/Notification.js
+++ Screens/StackScreen/Notification.js
@@ -1,25 +1,3 @@
-// import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
-// import React from 'react';
-// import { StatusBar } from 'expo-status-bar';
-// import { getDatabase, ref, set, onValue, push, update, remove } from "firebase/database";
-
-// export default function Notification() {
-//     return (
-//         <View style={styles.container}>
-//             <Text>Welcome to SquadSync Notification</Text>
-//         </View>
-//     );
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       backgroundColor: '#fff',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//     },
-// }); 
-
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState, useRef} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
